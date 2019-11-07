72e6d53f505f1a79688f47d7caa77405d0db8136
sruthi <sruthi@sruthi-ThinkPad-T440s>
2019-11-06 22:49:52 -0500

    staff - add severity changes

--- src/Staff.java
+++ src/Staff.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-import com.sun.org.apache.xerces.internal.impl.xpath.regex.ParseException;
+//import com.sun.org.apache.xerces.internal.impl.xpath.regex.ParseException;
 import java.util.concurrent.TimeUnit;
 import java.sql.*;
 import java.text.DateFormat;
@@ -33,11 +33,14 @@ public class Staff {
 			case 1:
 				checkedInPatientList();
 				break;
+			case 4:
+				selectSymptom();
+				break;
 			case 6:
 				System.out.println("Logging out..");
 				TimeUnit.SECONDS.sleep(3);
 				System.exit(0);
-				break;
+				break;	
 			default:
 				System.out.println("Invalid option,please enter a valid choice");
 				staffHome();
@@ -67,7 +70,7 @@ public class Staff {
 	System.out.println(ex);
 }
 		try {
-			System.out.println("--------------------------MENU PAGE ----------------------------");
+			System.out.println("-------------------------- MENU PAGE ----------------------------");
 			System.out.println("1. ENTER VITALS");
 			System.out.println("2. TREAT PATIENT");
 			System.out.println("3. GO BACK");
@@ -76,5 +79,105 @@ public class Staff {
 			System.out.println(ex);
 		}
 }
+
+	public void selectSymptom() {
+		try {
+			System.out.println("----------------------Symptom List-------------------");
+			Statement st = conn.createStatement();
+			ResultSet r = st.executeQuery("Select Symptom_name as name from symptom ");
+			int i = 1;
+			while (r.next()) {
+				System.out.println(i + ":-> " + r.getString("name"));
+				i++;
+			}
+			Scanner sc = new Scanner(System.in);
+			System.out.println("-------------------------- MENU PAGE ----------------------------");
+			System.out.println("1. ENTER SEVERITY");
+			System.out.println("2. GO BACK");
+			int choice = sc.nextInt();
+			System.out.println(choice);
+			switch (choice) {
+			case 1:
+				addSeverityScale();
+				break;
+			case 2:
+				staffHome();
+			default:
+				System.out.println("Invalid option,please enter a valid choice");
+				selectSymptom();
+				break;
+			}
+		
+			
+		}
+		catch(Exception ex) {
+			System.out.println(ex);
+		}
+	}
+	
+	public void addSeverityScale() {
+		try {
+		System.out.println("Please select a symptom to add scale");
+		Scanner sc = new Scanner(System.in);
+		String symptom = sc.nextLine();
+		PreparedStatement st = conn.prepareStatement("Select * from symptom where Symptom_name =?" );
+		st.setString(1, symptom);
+		ResultSet rs = st.executeQuery();
+		if (rs.next()) {
+				addScale(symptom);
+		}
+		}
+		catch(Exception ex) {
+			System.out.println(ex);
+		}
+		
+	}
+	
+	public void addScale(String symptom) throws SQLException {
+		System.out.println("----------------------Symptom Levels-------------------");
+		
+		PreparedStatement st = conn.prepareStatement("Select value as value from symptom_scale where Symptom_name =?");
+		st.setString(1, symptom);
+		ResultSet rs = st.executeQuery();
+		int i = 1;
+		while (rs.next()) {
+			System.out.println(i + ":-> " + rs.getString("value"));
+			i++;
+		}
+		System.out.println("-------------------------- MENU PAGE ----------------------------");
+		System.out.println("1. There is another level for the scale");
+		System.out.println("2. There are no more levels, Go Back");
+		Scanner sc = new Scanner(System.in);
+		int staff_choice = sc.nextInt();
+		switch (staff_choice) {
+		case 1:
+			addSeverityLevel(symptom);	
+		case 2:
+			selectSymptom();			
+		default:
+			System.out.println("Invalid option,please enter a valid choice");
+			addScale(symptom);
+			break;
+		}
+	 }
+	
+	
+	public void addSeverityLevel(String symptom){
+
+		try {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter Scale value");
+		String scale = scan.nextLine();
+		PreparedStatement stmt1 = conn.prepareStatement("insert into symptom_scale values(?, ?)");
+		stmt1.setString(1, symptom);
+		stmt1.setString(2, scale);
+		stmt1.executeUpdate();
+		System.out.println("Severity Added");
+		addScale(symptom);
+		}
+		catch(Exception ex) {
+			System.out.println(ex);
+		}
+	}
 }
 	
